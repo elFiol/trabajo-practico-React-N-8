b749836a6e93c3e62648dfc8b2f9e079a94b2057
Fiol Thiago <fiolthiago9@gmail.com>
2024-01-29 14:48:42 -0300

    añadimos el value en los campos y tambien agregamos para limpiar el formulario y de avisar si los datos estan correctos o no

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -26,14 +26,23 @@ const Formulario = () => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(value);
     };
-
+    const esNumero = (elemento) => {
+        console.log(true !== isNaN(elemento))
+        return true !== isNaN(elemento)
+    }
 
     const handlerForm = (e) => {
         e.preventDefault()
-        if (minYmax(nombre, 2, 45) && minYmax(apellido, 2, 45) && minYmax(DNI, 8, 8)) {
+        if (minYmax(nombre, 2, 45) && minYmax(apellido, 2, 45) && (minYmax(DNI, 8, 8) && esNumero(DNI)) && validarEmail(email)) {
             console.log("hola mundo")
+            setNombre("")
+            setApellido("")
+            setDNI("")
+            setEmail("")
+            alert("Datos Enviados")
         } else {
             console.log("NOOOOOOOOO")
+            alert("Datos incorrectos, intentelo de nuevo")
         }
     }
     return (
@@ -42,18 +51,18 @@ const Formulario = () => {
             <Form className="mx-5" onSubmit={handlerForm}>
                 <Form.Group className="my-3">
                     <Form.Label>Nombre:</Form.Label>
-                    <Form.Control type="text" minLength={2} maxLength={45} required placeholder="Ej:Juan" onChange={(e) => setNombre(e.target.value)}></Form.Control>
+                    <Form.Control type="text" minLength={2} maxLength={45} required placeholder="Ej:Juan" onChange={(e) => setNombre(e.target.value)} value={nombre}></Form.Control>
                 </Form.Group>
                 <Form.Group className="my-3">
                     <Form.Label>Apellido:</Form.Label>
-                    <Form.Control type="text" minLength={2} maxLength={45} required placeholder="Ej:Perez" onChange={(e) => setApellido(e.target.value)}></Form.Control>
+                    <Form.Control type="text" minLength={2} maxLength={45} required placeholder="Ej:Perez" onChange={(e) => setApellido(e.target.value)} value={apellido}></Form.Control>
                 </Form.Group><Form.Group className="my-3">
                     <Form.Label>DNI:</Form.Label>
-                    <Form.Control type="text" minLength={8} maxLength={8} required placeholder="Ej:49614369" onChange={(e) => setDNI(e.target.value)}></Form.Control>
+                    <Form.Control type="text" minLength={8} maxLength={8} required placeholder="Ej:49614369" onChange={(e) => setDNI(e.target.value)} value={DNI}></Form.Control>
                 </Form.Group>
                 <Form.Group className="my-3">
                     <Form.Label>Email:</Form.Label>
-                    <Form.Control type="email" required placeholder="Ej:Perez@gmail.com" onChange={(e) => setEmail(e.target.value)}></Form.Control>
+                    <Form.Control type="email" required placeholder="Ej:Perez@gmail.com" onChange={(e) => setEmail(e.target.value)} value={email}></Form.Control>
                 </Form.Group>
                 <Form.Group className="mt-5 d-flex justify-content-end">
                     <Button variant="light" type="submit">Enviar</Button>

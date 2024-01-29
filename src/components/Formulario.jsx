cdf3a4d6760b6a8f1094a3a71be661e503fd17b9
Fiol Thiago <fiolthiago9@gmail.com>
2024-01-29 14:02:16 -0300

    agregamos una parte de logica al componente Formulario, en este caso las validaciones (no terminado)

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,25 +1,62 @@
-import { Form } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 const Formulario = () => {
+    const [nombre, setNombre] = useState("")
+    const [apellido, setApellido] = useState("")
+    const [DNI, setDNI] = useState("")
+    const [email, setEmail] = useState("")
+
+    const minYmax = (elemento, min, max) => {
+        const texto = elemento.trim();
+        if (texto.length >= min && texto.length <= max) {
+            console.log(texto.length);
+            console.log(true)
+            return true;
+        } else {
+            console.log(texto.length);
+            console.log(false)
+            return false;
+        }
+    };
+
+    const validarEmail = (emailProp) => {
+        console.log(emailProp)
+        const value = emailProp
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(value);
+    };
+
+
+    const handlerForm = (e) => {
+        e.preventDefault()
+        if (minYmax(nombre, 2, 45) && minYmax(apellido, 2, 45) && minYmax(DNI, 8, 8)) {
+            console.log("hola mundo")
+        } else {
+            console.log("NOOOOOOOOO")
+        }
+    }
     return (
         <div className="text-light my-5 formulario py-4">
             <h2 className="text-center">Formulario</h2>
-            <Form className="mx-5">
+            <Form className="mx-5" onSubmit={handlerForm}>
                 <Form.Group className="my-3">
                     <Form.Label>Nombre:</Form.Label>
-                    <Form.Control type="text" minLength={2} maxLength={45} required placeholder="Ej:Juan"></Form.Control>
+                    <Form.Control type="text" minLength={2} maxLength={45} required placeholder="Ej:Juan" onChange={(e) => setNombre(e.target.value)}></Form.Control>
                 </Form.Group>
                 <Form.Group className="my-3">
                     <Form.Label>Apellido:</Form.Label>
-                    <Form.Control type="text" minLength={2} maxLength={45} required placeholder="Ej:Perez"></Form.Control>
-                </Form.Group>
-                <Form.Group className="my-3">
+                    <Form.Control type="text" minLength={2} maxLength={45} required placeholder="Ej:Perez" onChange={(e) => setApellido(e.target.value)}></Form.Control>
+                </Form.Group><Form.Group className="my-3">
                     <Form.Label>DNI:</Form.Label>
-                    <Form.Control type="number" min={11111111} max={99999999} required placeholder="Ej:49614369"></Form.Control>
+                    <Form.Control type="text" minLength={8} maxLength={8} required placeholder="Ej:49614369" onChange={(e) => setDNI(e.target.value)}></Form.Control>
                 </Form.Group>
                 <Form.Group className="my-3">
                     <Form.Label>Email:</Form.Label>
-                    <Form.Control type="email" required placeholder="Ej:Perez@gmail.com"></Form.Control>
+                    <Form.Control type="email" required placeholder="Ej:Perez@gmail.com" onChange={(e) => setEmail(e.target.value)}></Form.Control>
+                </Form.Group>
+                <Form.Group className="mt-5 d-flex justify-content-end">
+                    <Button variant="light" type="submit">Enviar</Button>
                 </Form.Group>
             </Form>
         </div>
